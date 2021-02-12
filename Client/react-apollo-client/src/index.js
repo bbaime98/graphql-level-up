426cd7f7bd03058274b1e9ca965097a804a19452
bien aimé <bybienaime@gmail.com>
2021-02-12 18:13:57 +0200

    set up apollo client in react
    - insatll apollo client and graphql in react
    - create apollo provider and connect with the backend graphql
    - use register user mutation

--- Client/react-apollo-client/src/index.js
+++ Client/react-apollo-client/src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
 
-const client = new ApolloClient({
-  uri: "http://localhost:3301/api"
-});
-
-ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
